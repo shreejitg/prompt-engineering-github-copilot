ab017881158371cd3b354e03c69d34fba3330bf5
Shreejit Gangadharan <sherryssj5@gmail.com>
2024-07-26 00:44:09 -0400

    Fixed the unit tests and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,38 @@
 import React, { useState } from 'react';
-import './App.css';
-import AddTodo from './AddTodo';
+import './App.css'; // Import the CSS file
 import TodoList from './TodoList';
+import AddTodo from './AddTodo';
 
-type Todo = {
-  id: number;
-  text: string;
-  completed: boolean;
-};
-
-function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  /**
-   * Adds a new todo to the list of todos.
-   * @param {string} text - The text of the todo.
-   */
-  const addTodo = (text: string) => {
-    const newTodo: Todo = {
-      id: Date.now(),
-      text,
-      completed: false,
-    };
-    setTodos([...todos, newTodo]);
-  };
-
-  /**
-   * Toggles the completion status of a todo item.
-   * @param id - The ID of the todo item to toggle.
-   */
-  const toggleTodo = (id: number) => {
-    setTodos(
-      todos.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  return (
-    <div className="App">
-      <h1>Todo List</h1>
-      <AddTodo addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
-    </div>
-  );
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
 }
+
+const App: React.FC = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const addTodo = (text: string) => {
+        const newTodo = { id: Date.now(), text, completed: false };
+        setTodos([...todos, newTodo]);
+    };
+
+    const toggleTodo = (id: number) => {
+        setTodos(todos.map(todo =>
+            todo.id === id ? { ...todo, completed: !todo.completed } : todo
+        ));
+    };
+
+    const deleteTodo = (id: number) => {
+        setTodos(todos.filter(todo => todo.id !== id));
+    };
+
+    return (
+        <div>
+            <AddTodo addTodo={addTodo} />
+            <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+        </div>
+    );
+};
 
 export default App;
